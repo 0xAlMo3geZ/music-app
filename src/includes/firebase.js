41d85c4c308f87354cee2eb1564b8cdd85e348b6
Muhammad Zaki <mahmadzekoo@gmail.com>
2023-07-08 19:36:41 +0300

    firebase config

--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -3,12 +3,14 @@ import 'firebase/auth';
 import 'firebase/firestore';
 import 'firebase/storage';
 
+// Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyAaSyK7W0_UWeDMTKiwTVcjTGZgX2qRkP8',
-  authDomain: 'music-c2596.firebaseapp.com',
-  projectId: 'music-c2596',
-  storageBucket: 'music-c2596.appspot.com',
-  appId: '1:417422649545:web:7439d0a2f274a0585ac518',
+  apiKey: 'AIzaSyAuWRtmtECx-uIBp4iMfj_Xn_OuMMp9aV0',
+  authDomain: 'music-app-e4ca5.firebaseapp.com',
+  projectId: 'music-app-e4ca5',
+  storageBucket: 'music-app-e4ca5.appspot.com',
+  messagingSenderId: '43059986651',
+  appId: '1:43059986651:web:0f9b8a6f6f1dccd7ae0cfe',
 };
 
 // Initialize Firebase
